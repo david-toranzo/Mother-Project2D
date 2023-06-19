43c6f6a09c4f6b51b5cee0a2b588dbf00dfc4c54
david444456 <david-toranzo-@hotmail.com>
2023-06-19 20:55:37 -0300

    [A] add sprites shapes

--- Assets/Character/Common/Scripts/Controllers/CharacterMediator.cs
+++ Assets/Character/Common/Scripts/Controllers/CharacterMediator.cs
@@ -9,8 +9,10 @@ namespace Runtime.Character2D
         public void ChangeStateCharacterActive(bool newState)
         {
             _playerMovement.enabled = newState;
+
+            //delete this
             _playerMovement.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-            _playerMovement.GetComponent<Animator>().Play("Iddle");
+            _playerMovement.GetComponent<Animator>().SetFloat("Speed", 0);
         }
     }
 }
