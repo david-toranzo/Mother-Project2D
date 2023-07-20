df87b18f9696297b1d811d8052c350344f6a4009
david444456 <david-toranzo-@hotmail.com>
2023-07-20 00:48:33 -0300

    [A] add attack character notifier

---
+++ Assets/Character/Others/Scripts/CharacterAttackEventNotifier.cs
@@ -0,0 +1,29 @@
+﻿using ScriptableObjects.Event;
+using UnityEngine;
+
+namespace Runtime.Character2D
+{
+    public class CharacterAttackEventNotifier : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private CharacterFighter _characterFighter;
+
+        [Header("Data")]
+        [SerializeField] private EmptyEventScriptableObject _eventScriptableObject;
+
+        private void Start()
+        {
+            _characterFighter.OnAttackFighter += MakeAttack;
+        }
+
+        private void MakeAttack()
+        {
+            _eventScriptableObject.InvokeEvent();
+        }
+
+        private void OnDestroy()
+        {
+            _characterFighter.OnAttackFighter -= MakeAttack;
+        }
+    }
+}
