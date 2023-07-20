cb6c8f0aa12dcc4ae6609973cf34251ee77c9356
david444456 <david-toranzo-@hotmail.com>
2023-07-20 15:09:24 -0300

    [A] add event dead enemy and sound enemy

---
+++ Assets/Common/Scripts/Stats/View/DieEventEmptyNotifier.cs
@@ -0,0 +1,28 @@
+﻿using ScriptableObjects.Event;
+using UnityEngine;
+
+namespace Common
+{
+    public class DieEventEmptyNotifier : MonoBehaviour
+    {
+        [SerializeField] private HealthController _healthController;
+
+        [Header("Data")]
+        [SerializeField] private EmptyEventScriptableObject _eventScriptableObject;
+
+        private void Start()
+        {
+            _healthController.OnDie += EventNotify;
+        }
+
+        private void EventNotify()
+        {
+            _eventScriptableObject.InvokeEvent();
+        }
+
+        private void OnDestroy()
+        {
+            _healthController.OnDie -= EventNotify;
+        }
+    }
+}
---
+++ Assets/Common/Scripts/Stats/View/HitEventEmptyNotifier.cs
@@ -0,0 +1,28 @@
+﻿using ScriptableObjects.Event;
+using UnityEngine;
+
+namespace Common
+{
+    public class HitEventEmptyNotifier : MonoBehaviour
+    {
+        [SerializeField] private HealthController _healthController;
+
+        [Header("Data")]
+        [SerializeField] private EmptyEventScriptableObject _eventScriptableObject;
+
+        private void Start()
+        {
+            _healthController.OnSubstractHealth += EventNotify;
+        }
+
+        private void EventNotify(int value)
+        {
+            _eventScriptableObject.InvokeEvent();
+        }
+
+        private void OnDestroy()
+        {
+            _healthController.OnSubstractHealth -= EventNotify;
+        }
+    }
+}
